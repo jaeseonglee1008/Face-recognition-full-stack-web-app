fab926dc9bbd2e73243e6fed420e91593bad7e06
jason <jason@jasons-MacBook-Pro.local>
2018-11-30 07:49:32 -0500

    -aupdated UI color, added portfolio style, gallery img alignment

--- blog/views.py
+++ blog/views.py
@@ -13,12 +13,12 @@ def post_list(request):
 def uimage(request):
 	if request.method == 'POST':
 		form = UploadImageForm(request.POST, request.FILES)
-	if form.is_valid():
-		myfile = request.FILES['image']
-		fs = FileSystemStorage()
-		filename = fs.save(myfile.name, myfile)
-		uploaded_file_url = fs.url(filename)
-		return render(request, 'opencv_webapp/uimage.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
+		if form.is_valid():
+			myfile = request.FILES['image']
+			fs = FileSystemStorage()
+			filename = fs.save(myfile.name, myfile)
+			uploaded_file_url = fs.url(filename)
+			return render(request, 'blog/uimage.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
 	else:
 		form = UploadImageForm()
-		return render(request, 'opencv_webapp/uimage.html', {'form': form})
+		return render(request, 'blog/uimage.html', {'form': form})
