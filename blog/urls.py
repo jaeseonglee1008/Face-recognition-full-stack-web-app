9728beba22baf9653f5a148c7d3b1c573cc2c39f
jason <jason@jasons-MacBook-Pro.local>
2018-11-30 18:10:33 -0500

    changed regex

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^uimage/$', views.uimage, name='uimage'),
     url(r'^dface/$', views.dface, name='dface'),
+    # url(r'^uimage/$', views.uimage, name='uimage'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
