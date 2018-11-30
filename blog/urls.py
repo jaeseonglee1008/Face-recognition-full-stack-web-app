fab70b9e5168a8c67b2b42d344349282c1120ac9
jason <jason@jasons-MacBook-Pro.local>
2018-11-30 06:41:00 -0500

    created upload image form class(text form and img form)|imported by views.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^uimage/$', views.uimage, name='uimage'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
-# Create your views here.
-# def post_list(request):
-#     return render(request, 'blog/post_list.html', {})
+
+from django.shortcuts import redirect
+from .forms import UploadImageForm
+from django.core.files.storage import FileSystemStorage
+
+
+
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts':posts})
+	return render(request, 'blog/post_list.html', {})
+
+def uimage(request):
+	if request.method == 'POST':
+		form = UploadImageForm(request.POST, request.FILES)
+	if form.is_valid():
+		myfile = request.FILES['image']
+		fs = FileSystemStorage()
+		filename = fs.save(myfile.name, myfile)
+		uploaded_file_url = fs.url(filename)
+		return render(request, 'opencv_webapp/uimage.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
+	else:
+		form = UploadImageForm()
+		return render(request, 'opencv_webapp/uimage.html', {'form': form})
