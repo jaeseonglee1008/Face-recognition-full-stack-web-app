81203dbdc6158e514ff24b039cc562f213930fad
jason <jason@jasons-MacBook-Pro.local>
2018-12-01 04:46:07 -0500

    databse deletion function/superuser

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import ImageUploadModel
+# allows to check uploaded img
+class upload_image_Admin(admin.ModelAdmin):
+  list_display = ('description', 'document')
+
+admin.site.register(ImageUploadModel, upload_image_Admin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+# Receive the pre_delete signal and delete the file associated with the model instance.
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 class ImageUploadModel(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.ImageField(upload_to='images/%Y/%m/%d')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+@receiver(pre_delete, sender=ImageUploadModel)
+def mymodel_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.document.delete(False)
