bab30855689c8b5c639e0a06f7a10251c60d0ee8
jason <jason@jasons-MacBook-Pro.local>
2018-12-05 13:41:34 -0500

    added style for face recog main, set id and class for form attribute, updated scale factor and minNeighbor

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from .models import ImageUploadModel
-
+# form field. gon add styling later
 class UploadImageForm(forms.Form):
     title = forms.CharField(max_length=50)
     image = forms.ImageField()
-
+# title meta
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = ImageUploadModel
+        # fields = "__all__"
         fields = ('description', 'document')
+    # set attributes form style
+    def __init__( self, *args, **kwargs ):
+
+        super( ImageUploadForm, self ).__init__( *args, **kwargs )
+        self.fields[ 'description' ].widget.attrs.update({'class': 'form-control','id': 'description-id','placeholder': 'Who\'s in this picture?' } )
+        self.fields[ 'document' ].widget.attrs.update({'class': 'form-control', 'id': 'document-id'} )
+
+# form styling
+# class MyForm(forms.Form):
+#     myfield = forms.CharField(widget=forms.TextInput(attrs={'class' : 'myfieldclass'}))
--- blog/opencv_dface.py
+++ blog/opencv_dface.py
@@ -29,15 +29,16 @@ def opencv_dface(path):
         # input img converted to gray
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # face detection with the adaboost cascade algorithm
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        # scale factor 1.2, minNeighbor
+        faces = face_cascade.detectMultiScale(gray, 1.3, 10)
         # draw
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (94, 73, 52), 10)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
+            eyes = eye_cascade.detectMultiScale(roi_gray, 1.2, 5)
             for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (219, 152, 52), 10)
         # overwrite result
         cv2.imwrite(path, img)
 
